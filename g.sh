d4aa10c7de5ea5242c4f5bea99a7e711df605e74
root <root@tests.1.1.1.1>
2025-08-25 14:26:39 +0000

    chore

--- g.sh
+++
@@ -1,6 +0,0 @@
-git add .
-git commit -m "refactor: Align project with Ansible best practices" -m "
-- Secure secrets using ansible-vault
-- Move role variables to defaults/main.yml
-- Implement file-based logging for Xray
-- Replace helper scripts with a dedicated playbook"
